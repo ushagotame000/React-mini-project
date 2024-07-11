0c7058e162c71eca51c6b1f7c87569dd79035da4
Usha Gotame <ushagotame000@gmail.com>
2024-07-11 11:30:47 +0545

    tic-tac-toe complete

--- src/component/TicTacToe/TicTacToe.jsx
+++ src/component/TicTacToe/TicTacToe.jsx
@@ -1,117 +1,98 @@
-import React, { useState } from 'react'
-import assets from '../assets/assets'
+import React, { useState, useRef } from 'react';
+import assets from '../assets/assets';
 
-
-
-let data = ["","","","","","","","",""];
-
+let initialData = ["", "", "", "", "", "", "", "", ""];
 
 export const TicTacToe = () => {
-  let [count,setCount] = useState("0");
-  let [lock,setLock] = useState(false);
-  let titleRef = useRef(null);
+  const [count, setCount] = useState(0);
+  const [lock, setLock] = useState(false);
+  const [data, setData] = useState(initialData);
+  const titleRef = useRef(null);
 
-
-const toggle = (e,num) =>{
-  if (lock){
-    return 0;
-  }
-  if (count % 2 === 0){
-    e.target.innerHTML = `<img src = '${assets[0]}'>`;
-    data[num] = "x";
-    setCount (++count)
-  }
-  else{
-    e.target.innerHTML = `<img src = '${assets[1]}'>`;
-    data[num] = "o";
-    setCount (++count)
-  }
-  checkWin ();
-}
-
-
-  //check winor lose
-  const checkWin = () =>{
-    if (data[0] === data[1] && data[1]===data[3] && data [2]!==""){
-      won(data[2]);
+  const toggle = (e, num) => {
+    if (lock || data[num]) {
+      return;
     }
-    else if (data[3] === data[4] && data[4]===data[5] && data [5]!==""){
-won(data[5]); 
-  }
+    const newData = [...data];
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src='${assets[0]}'>`;
+      newData[num] = "x";
+    } else {
+      e.target.innerHTML = `<img src='${assets[1]}'>`;
+      newData[num] = "o";
+    }
+    setData(newData);
+    setCount(count + 1);
+    checkWin(newData);
+  };
 
-  else if (data[6] === data[7] && data[7]===data[8] && data [8]!==""){
-    won(data[8]); 
-      }
-      else if (data[0] === data[3] && data[3]===data[6] && data [6]!==""){
-        won(data[6]);
-      }
-      else if (data[1] === data[4] && data[4]===data[7] && data [7]!==""){
-        won(data[7]);
-      }
-      else if (data[2] === data[5] && data[5]===data[8] && data [8]!==""){
-        won(data[8]);
-      }
-      else if (data[0] === data[4] && data[4]===data[8] && data [8]!==""){
-        won(data[8]);
-      }
-      else if (data[0] === data[1] && data[1]===data[2] && data [2]!==""){
-        won(data[2]);
-      }
-      else  if (data[2] === data[4] && data[4]===data[6] && data [6]!==""){
-        won(data[6]);
-      }
-      
+  const checkWin = (newData) => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (newData[a] && newData[a] === newData[b] && newData[a] === newData[c]) {
+        won(newData[a]);
+        return;
+      }
+    }
+  };
 
-  const won = (winner) =>{
+  const won = (winner) => {
     setLock(true);
-    if (winner==="x"){
-      titleRef.current.innerHTML = `Congratulations: <img src = ${assets[0]}`>
+    if (winner === "x") {
+      titleRef.current.innerHTML = `Congratulations:  <img  src='${assets[0]}' Winner class='w-16 bg-gray-100 rounded-full'> Winner`;
+    } else {
+      titleRef.current.innerHTML = `Congratulations:   <img src='${assets[1]}' class='w-16 bg-gray-100 rounded-full' >Winner`;
     }
-    else{
-      titleRef.current.innerHTML = `Congratulations: <img src = ${assets[1]}`>
+  };
 
-    }
-  }
-
-};
+  const resetGame = () => {
+    setData(initialData);
+    setCount(0);
+    setLock(false);
+    titleRef.current.innerHTML = 'Tic Tac Toe Game <span className="text-green-600">-In React</span>';
+    document.querySelectorAll('.boxes').forEach(box => box.innerHTML = '');
+  };
 
   return (
-
-<>
-<div className="container  justify-center">
-    <h1 className='mt-12 font-bold white text-xl flex justify-center items-center' ref={titleRef}>Tic Tac Toe Game <span className='text-green-600'>-In React</span></h1>
-    <div className="board  flex m-auto justify-center">
-      <div className="row1">
-        <div  onClick={(e)=>{toggle(e,0)}}  className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer "></div>
-        <div onClick={(e)=>{toggle(e,1)}}  className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
-        <div  onClick={(e)=>{toggle(e,2)}} className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
+    <>
+      <div className="container justify-center bg-black h-screen w-screen">
+        <h1 className=" text-white p-10 font-bold white text-3xl flex justify-center items-center" ref={titleRef}>
+          Tic Tac Toe Game <span className="text-green-500">-In React</span>
+        </h1>
+        <div className="board flex m-auto justify-center mt-9">
+          <div className="row1">
+            {[0, 1, 2].map(num => (
+              <div key={num} onClick={(e) => toggle(e, num)} className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
+            ))}
+          </div>
+          <div className="row2">
+            {[3, 4, 5].map(num => (
+              <div key={num} onClick={(e) => toggle(e, num)} className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
+            ))}
+          </div>
+          <div className="row3">
+            {[6, 7, 8].map(num => (
+              <div key={num} onClick={(e) => toggle(e, num)} className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
+            ))}
+          </div>
+        </div>
+        <div className="button flex justify-center pt-4">
+          <button onClick={resetGame} className="reset w-16 h-7 border-none outline-none cursor-pointer rounded-md bg-purple-400 font-medium text-xl text-white ">
+            Reset
+          </button>
+        </div>
       </div>
-      <div className="row2">
-        <div  onClick={(e)=>{toggle(e,3)}} className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
-        <div  onClick={(e)=>{toggle(e,4)}} className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
-        <div  onClick={(e)=>{toggle(e,5)}} className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
-      </div>
-
-
-      <div className="row3"> 
-        <div onClick={(e)=>{toggle(e,6)}}  className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
-        <div  onClick={(e)=>{toggle(e,7)}} className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
-        <div  onClick={(e)=>{toggle(e,8)}} className="boxes flex h-44 w-44 bg-purple-400 rounded-sm border-4 cursor-pointer"></div>
-      </div>
-
-    </div>
-<div className="button ">
-   <button className="reset  w-16 h-7 border-none outline-none cursor-pointer rounded-md bg-gray-400 font-medium text-xl text-green-300">
-      Reset
-    </button>
-</div>
-   
-</div>
-</>
-
-    // <div>TicTacToe
-    //     <img src={assets[0]} alt="circle" />
-    // </div>
+    </>
   );
 };
